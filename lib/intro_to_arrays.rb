833ded4bdd8953af10024958f0e547b46d3809af
Kolton Starr <starrkolton@gmail.com>
2019-04-29 14:28:34 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,44 @@
+def instantiate_new_array
+  list = [ ]
+end 
+
+def array_with_two_elements
+  list2 = ["Mom", "Dad"]
+end 
+
+def first_element(array)
+  array[0] 
+end 
+
+def third_element(array)
+  array[2]
+end 
+
+def last_element(array) 
+  array[-1]
+end 
+
+def first_element_with_array_methods(array)
+  array.first
+end 
+
+
+def last_element_with_array_methods(array)
+  array.last 
+end 
+
+def length_of_array(array)
+  array.length 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
